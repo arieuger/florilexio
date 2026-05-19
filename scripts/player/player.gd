7c54ed15989645fe3336b83bdfc9d8a7c4119069
A. Roig <antiaroig@gmail.com>
2026-05-19 21:48:44 +0200

    Animación sinxela xogador

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,9 +1,28 @@
 extends CharacterBody2D
 
-@export var speed = 60.0
+@export var max_speed := 60.0
+@export var acceleration := 420.0
+@export var deceleration := 520.0
 
-func _physics_process(delta: float) -> void:
-    var input_direction = Input.get_vector("left", "right", "up", "down")
-    velocity = input_direction * speed
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var _last_facing := "down"
+
+func _physics_process(delta: float):
+    var input_direction := Input.get_vector("left", "right", "up", "down")
+    var target_velocity := input_direction * max_speed  
+    if input_direction != Vector2.ZERO:
+        velocity = velocity.move_toward(target_velocity, acceleration * delta)
+    else:
+        velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+
     move_and_slide()
 
+    if input_direction == Vector2.ZERO:
+        animated_sprite.play("idle_" + _last_facing)
+    elif input_direction.y < 0:
+        _last_facing = "up"
+        animated_sprite.play("walk_up")
+    else:
+        _last_facing = "down"
+        animated_sprite.play("walk_down")
