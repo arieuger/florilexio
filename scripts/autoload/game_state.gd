db0c89565fef828737d138f66977f0a432d3d480
edupoch <edupoch@gmail.com>
2026-05-26 14:19:11 +0200

    Addon de FMOD e proba de que a música soa

--- scripts/autoload/game_state.gd
+++ scripts/autoload/game_state.gd
@@ -24,34 +24,34 @@ var _consumed_time: int = 0 # Bloques de 15 minutos: en 12 horas, 48 bloques
 # tóxicas, invasoras, p. de extición, máxicas (s. xoán, básicas e outras), outras
 
 var consumed_time: int:
-    get:
-        return _consumed_time
+	get:
+		return _consumed_time
 
 func get_current_time_minutes() -> int:
-    var clamped_blocks = clampi(_consumed_time, 0, TOTAL_BLOCKS)
-    return (START_HOUR * 60) + (clamped_blocks * BLOCK_MINUTES)
+	var clamped_blocks = clampi(_consumed_time, 0, TOTAL_BLOCKS)
+	return (START_HOUR * 60) + (clamped_blocks * BLOCK_MINUTES)
 
 func get_current_time_text() -> String:
-    var current_minutes := get_current_time_minutes()
-    var hour := int(current_minutes / 60)
-    var minute := current_minutes % 60
-    return "%02d:%02d" % [hour, minute]
+	var current_minutes := get_current_time_minutes()
+	var hour := int(current_minutes / 60)
+	var minute := current_minutes % 60
+	return "%02d:%02d" % [hour, minute]
 
 func add_consumed_time(time: int) -> void:
-    _consumed_time += time
+	_consumed_time += time
 
-    consumed_time_added.emit(_consumed_time)
-    # TODO: Actualizar UI según o tempo consumido e reacción ao cambio
-    print("Consumed time: " + str(_consumed_time) + " seconds")
+	consumed_time_added.emit(_consumed_time)
+	# TODO: Actualizar UI según o tempo consumido e reacción ao cambio
+	print("Consumed time: " + str(_consumed_time) + " seconds")
 
 func discover_plant(plant_id: String) -> void:
-    if not discovered_plants.has(plant_id):
-        discovered_plants[plant_id] = true
+	if not discovered_plants.has(plant_id):
+		discovered_plants[plant_id] = true
 
 func collect_plant(plant_id: String) -> bool:
-    if discovered_plants.has(plant_id):
-        collected_plants[plant_id] = true
-        return true
-    else:
-        print("Cannot collect plant that has not been discovered: " + plant_id)
-        return false
+	if discovered_plants.has(plant_id):
+		collected_plants[plant_id] = true
+		return true
+	else:
+		print("Cannot collect plant that has not been discovered: " + plant_id)
+		return false
--- scripts/general/world_time_controller.gd
+++ scripts/general/world_time_controller.gd
@@ -9,32 +9,32 @@ var _color_tween: Tween
 var _current_world_color: Color
 
 func _ready() -> void:
-    GameState.consumed_time_added.connect(_on_consumed_time_added)
-    _current_world_color = _get_world_color(GameState.consumed_time)
-    color = _current_world_color
+	GameState.consumed_time_added.connect(_on_consumed_time_added)
+	_current_world_color = _get_world_color(GameState.consumed_time)
+	color = _current_world_color
 
 func _on_consumed_time_added(total_consumed_time: int) -> void:
-    var target_color = _get_world_color(total_consumed_time)
-    if target_color == _current_world_color: return
+	var target_color = _get_world_color(total_consumed_time)
+	if target_color == _current_world_color: return
 
-    _current_world_color = target_color
-    _tween_world_color(target_color)
+	_current_world_color = target_color
+	_tween_world_color(target_color)
 
 func _get_world_color(consumed_time: int) -> Color:
-    var clamped_blocks = clamp(consumed_time, 0, GameState.TOTAL_BLOCKS)
-    var progress = float(clamped_blocks) / float(GameState.TOTAL_BLOCKS)
-    if progress < 0.3:
-        return morning_color
-    elif progress < 0.6:
-        return afternoon_color
-    elif progress < 0.85:
-        return sunset_color
-    else:        
-        return night_color
+	var clamped_blocks = clamp(consumed_time, 0, GameState.TOTAL_BLOCKS)
+	var progress = float(clamped_blocks) / float(GameState.TOTAL_BLOCKS)
+	if progress < 0.3:
+		return morning_color
+	elif progress < 0.6:
+		return afternoon_color
+	elif progress < 0.85:
+		return sunset_color
+	else:        
+		return night_color
 
 func _tween_world_color(target_color: Color) -> void:
-    if _color_tween: _color_tween.kill()
-    
-    _color_tween = create_tween()
-    _color_tween.tween_property(self, "color", target_color, 1.2).\
-        set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	if _color_tween: _color_tween.kill()
+	
+	_color_tween = create_tween()
+	_color_tween.tween_property(self, "color", target_color, 1.2).\
+		set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
