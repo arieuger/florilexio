7ee432aba9419686624d71194faf5a12ddd5b868
A. Roig <antia.roig@cuatrodigital.com>
2026-05-29 13:52:05 +0200

    Fix en modo día/noite

--- scripts/interactables/night_changer.gd
+++ scripts/interactables/night_changer.gd
@@ -4,12 +4,15 @@ extends CanvasItem
 
 func _ready():
 	GameState.reached_night.connect(_on_reached_night)
-	if GameState.consumed_time >= GameState.NIGHT_START_BLOCK:
-		_on_reached_night()
+	_apply_night_state(GameState.consumed_time >= GameState.NIGHT_START_BLOCK)
 
 func _on_reached_night():
-	visible = visible_at_night
-	process_mode = Node.PROCESS_MODE_INHERIT if visible_at_night \
-		else Node.PROCESS_MODE_DISABLED
-	if has_node("NightSound"):
+	_apply_night_state(true)
+
+func _apply_night_state(is_night: bool) -> void:
+	var should_be_visible := visible_at_night == is_night
+	visible = should_be_visible
+	process_mode = Node.PROCESS_MODE_INHERIT if should_be_visible else Node.PROCESS_MODE_DISABLED
+
+	if is_night and visible_at_night and has_node("NightSound"):
 		get_node("NightSound").play()
