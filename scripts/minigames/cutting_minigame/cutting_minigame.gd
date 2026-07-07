9239daa07ee46685ceca934ec92a00f55335b5b6
A. Roig <antiaroig@gmail.com>
2026-07-08 00:16:02 +0200

    Xeralización feedback minixogo

--- scripts/minigames/cutting_minigame/cutting_minigame.gd
+++ scripts/minigames/cutting_minigame/cutting_minigame.gd
@@ -23,10 +23,7 @@ var success_alpha_threshold := 0.1
 @onready var cursor: Sprite2D = $GameRoot/CursorPivot/Cursor
 @onready var success_zones: Sprite2D = $GameRoot/SuccessZones
 @onready var feedback_label: Label = $FeedbackLabel
-
-@onready var _cut_progress_bar: TextureProgressBar = $UIContainer/ControlProgressBox/CutProgressBar
-@onready var _fail_progress_bar: TextureProgressBar = $UIContainer/ControlProgressBox/FailProgressBar
-@onready var _time_cost_label: Label = $UIContainer/TimeContainer/TimeLabel
+@onready var status_panel = $UIContainer
 
 var _current_angle := 0.0
 var _rotation_direction := 1.0
@@ -37,10 +34,6 @@ var _feedback_tween: Tween
 var _success_zones_image: Image
 var movingGrassSound : FmodEvent
 
-var _cut_progress_tween: Tween
-var _fail_progress_tween: Tween
-var _fail_bar_shake_tween: Tween
-
 func _ready() -> void:
 	add_to_group("cutting_minigame")
 	get_viewport().size_changed.connect(_center_game)
@@ -48,9 +41,8 @@ func _ready() -> void:
 	cursor.position = Vector2(cursor_radius, 0.0)
 	feedback_label.text = ""
 	_cache_success_zones_image()
+	status_panel.setup(required_hits, max_misses, time_cost_blocks, miss_time_cost_blocks)
 	movingGrassSound = SoundManager.play_looped_sound('Minigame/Moving Grass')
-	_setup_progress_bars()
-	_update_time_cost_label()
 
 
 func _process(delta: float) -> void:
@@ -105,7 +97,7 @@ func _try_cut() -> void:
 		_hits += 1
 		_show_feedback("%s %d/%d" % [tr("Ben!"), _hits, required_hits], Color(0.6, 1.0, 0.6, 1.0), Color('#6f8f6f'))
 		_pulse_success_zones()
-		_animate_cut_progress_bar()
+		status_panel.set_hits(_hits)
 		SoundManager.play_simple_sound("Minigame/Cutting Grass")
 		if _hits >= required_hits:
 			_finish(true)
@@ -114,10 +106,8 @@ func _try_cut() -> void:
 		_misses += 1
 		request_time_cost(miss_time_cost_blocks)
 		_show_feedback("%s %d/%d" % [tr("Fallaches"), _misses, max_misses], Color(1.0, 0.45, 0.45, 1.0), Color('#db5968'))
-		_update_time_cost_label(true)
 		_shake_ring()
-		_animate_fail_progress_bar()
-		_shake_fail_bar()
+		status_panel.set_misses(_misses)
 		SoundManager.play_simple_sound('Actions/Error')
 		if _misses >= max_misses:
 			_finish(false)
@@ -311,93 +301,3 @@ func _shake_ring() -> void:
 
 func _center_game() -> void:
 	game_root.position = get_viewport().get_visible_rect().size * 0.5
-
-func _setup_progress_bars() -> void:
-	_cut_progress_bar.max_value = required_hits
-	_cut_progress_bar.min_value = 0
-	_cut_progress_bar.value = _hits
-
-	_fail_progress_bar.max_value = max_misses
-	_fail_progress_bar.min_value = 0
-	_fail_progress_bar.value = _misses
-
-func _animate_cut_progress_bar() -> void:
-	_cut_progress_tween = _animate_progress_bar(
-		_cut_progress_bar,
-		_cut_progress_tween,
-		float(_hits)
-	)
-
-
-func _animate_fail_progress_bar() -> void:
-	_fail_progress_tween = _animate_progress_bar(
-		_fail_progress_bar,
-		_fail_progress_tween,
-		float(_misses)
-	)
-
-
-func _animate_progress_bar(
-	bar: TextureProgressBar,
-	current_tween: Tween,
-	target_value: float
-) -> Tween:
-	if current_tween:
-		current_tween.kill()
-
-	var start_modulate := bar.self_modulate
-	var flash_modulate := Color(2.5, 2.5, 2.5, 1.0)
-	var overshoot := minf(target_value + 0.12, bar.max_value)
-
-	var tween := create_tween()
-
-	tween.tween_property(bar, "value", target_value, 0.18)\
-		.set_trans(Tween.TRANS_SINE)\
-		.set_ease(Tween.EASE_OUT)
-
-	tween.parallel().tween_property(bar, "self_modulate", flash_modulate, 0.05)
-
-	tween.tween_property(bar, "value", overshoot, 0.07)\
-		.set_trans(Tween.TRANS_QUAD)\
-		.set_ease(Tween.EASE_OUT)
-
-
-	tween.tween_property(bar, "value", target_value, 0.12)\
-		.set_trans(Tween.TRANS_SINE)\
-		.set_ease(Tween.EASE_OUT)
-
-	tween.parallel().tween_property(bar, "self_modulate", start_modulate, 0.16)
-
-	return tween
-
-
-func _shake_fail_bar() -> void:
-	if _fail_bar_shake_tween:
-		_fail_bar_shake_tween.kill()
-
-	var start_position := _fail_progress_bar.position
-	_fail_bar_shake_tween = create_tween()
-	_fail_bar_shake_tween.tween_property(_fail_progress_bar, "position", start_position + Vector2(1.5, 0), 0.035)
-	_fail_bar_shake_tween.tween_property(_fail_progress_bar, "position", start_position + Vector2(-1.5, 0), 0.035)
-	_fail_bar_shake_tween.tween_property(_fail_progress_bar, "position", start_position + Vector2(1.0, 0), 0.03)
-	_fail_bar_shake_tween.tween_property(_fail_progress_bar, "position", start_position, 0.04)
-
-func _update_time_cost_label(animate := false) -> void:
-	var total_blocks := time_cost_blocks + (float(_misses) * miss_time_cost_blocks)
-	var total_minutes := roundi(total_blocks * GameState.BLOCK_MINUTES)
-	_time_cost_label.text = "+%d min" % total_minutes
-	if animate: _shake_time_cost()
-
-func _shake_time_cost() -> void:
-	var start_position := _time_cost_label.position
-	var base_color := _time_cost_label.modulate
-	var flash_color := Color(2.5, 2.5, 2.5, 1.0)
-
-	var tween := create_tween()
-	tween.tween_property(_time_cost_label, "position", start_position + Vector2(1.5, 0), 0.035)
-	tween.tween_property(_time_cost_label, "position", start_position + Vector2(-1.5, 0), 0.035)
-	tween.tween_property(_time_cost_label, "position", start_position + Vector2(1.0, 0), 0.03)
-	tween.tween_property(_time_cost_label, "position", start_position, 0.04)
-
-	tween.parallel().tween_property(_time_cost_label, "modulate", flash_color, 0.06)
-	tween.tween_property(_time_cost_label, "modulate", base_color, 0.16)
---
+++ scripts/ui/minigame_status_panel.gd
@@ -0,0 +1,117 @@
+extends VBoxContainer
+class_name MinigameStatusPanel
+
+@onready var _hit_progress_bar: TextureProgressBar = $ControlProgressBox/HitProgressBar
+@onready var _miss_progress_bar: TextureProgressBar = $ControlProgressBox/MissProgressBar
+@onready var _time_cost_label: Label = $TimeContainer/TimeLabel
+
+var _time_cost_blocks := 0.0
+var _miss_time_cost_blocks := 0.0
+var _hits := 0
+var _misses := 0
+var _hit_progress_tween: Tween
+var _miss_progress_tween: Tween
+var _miss_bar_shake_tween: Tween
+
+
+func setup(
+	required_hits: int,
+	max_misses: int,
+	time_cost_blocks: float,
+	miss_time_cost_blocks: float
+) -> void:
+	_time_cost_blocks = time_cost_blocks
+	_miss_time_cost_blocks = miss_time_cost_blocks
+	_hits = 0
+	_misses = 0
+
+	_setup_progress_bar(_hit_progress_bar, required_hits)
+	_setup_progress_bar(_miss_progress_bar, max_misses)
+	_update_time_cost_label()
+
+
+func set_hits(hits: int, animate := true) -> void:
+	_hits = hits
+	if animate:
+		_hit_progress_tween = _animate_progress_bar(_hit_progress_bar, _hit_progress_tween, float(_hits))
+	else:
+		_hit_progress_bar.value = _hits
+
+
+func set_misses(misses: int, animate := true) -> void:
+	_misses = misses
+	if animate:
+		_miss_progress_tween = _animate_progress_bar(_miss_progress_bar, _miss_progress_tween, float(_misses))
+		_shake_miss_bar()
+	else:
+		_miss_progress_bar.value = _misses
+	_update_time_cost_label(animate)
+
+
+func _setup_progress_bar(bar: TextureProgressBar, max_value: int) -> void:
+	bar.min_value = 0.0
+	bar.max_value = max_value
+	bar.step = 0.0
+	bar.value = 0.0
+	bar.self_modulate = Color.WHITE
+
+
+func _animate_progress_bar(
+	bar: TextureProgressBar,
+	current_tween: Tween,
+	target_value: float
+) -> Tween:
+	if current_tween:
+		current_tween.kill()
+
+	var start_modulate := bar.self_modulate
+	var flash_modulate := Color(2.5, 2.5, 2.5, 1.0)
+	var overshoot := minf(target_value + 0.12, bar.max_value)
+
+	var tween := create_tween()
+	tween.tween_property(bar, "value", target_value, 0.18)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(bar, "self_modulate", flash_modulate, 0.05)
+	tween.tween_property(bar, "value", overshoot, 0.07)\
+		.set_trans(Tween.TRANS_QUAD)\
+		.set_ease(Tween.EASE_OUT)
+	tween.tween_property(bar, "value", target_value, 0.12)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(bar, "self_modulate", start_modulate, 0.16)
+	return tween
+
+
+func _shake_miss_bar() -> void:
+	if _miss_bar_shake_tween:
+		_miss_bar_shake_tween.kill()
+
+	var start_position := _miss_progress_bar.position
+	_miss_bar_shake_tween = create_tween()
+	_miss_bar_shake_tween.tween_property(_miss_progress_bar, "position", start_position + Vector2(1.5, 0), 0.035)
+	_miss_bar_shake_tween.tween_property(_miss_progress_bar, "position", start_position + Vector2(-1.5, 0), 0.035)
+	_miss_bar_shake_tween.tween_property(_miss_progress_bar, "position", start_position + Vector2(1.0, 0), 0.03)
+	_miss_bar_shake_tween.tween_property(_miss_progress_bar, "position", start_position, 0.04)
+
+
+func _update_time_cost_label(animate := false) -> void:
+	var total_blocks := _time_cost_blocks + (float(_misses) * _miss_time_cost_blocks)
+	var total_minutes := roundi(total_blocks * GameState.BLOCK_MINUTES)
+	_time_cost_label.text = "+%d min" % total_minutes
+	if animate:
+		_shake_time_cost()
+
+
+func _shake_time_cost() -> void:
+	var start_position := _time_cost_label.position
+	var base_color := _time_cost_label.modulate
+	var flash_color := Color(2.5, 2.5, 2.5, 1.0)
+
+	var tween := create_tween()
+	tween.tween_property(_time_cost_label, "position", start_position + Vector2(1.5, 0), 0.035)
+	tween.tween_property(_time_cost_label, "position", start_position + Vector2(-1.5, 0), 0.035)
+	tween.tween_property(_time_cost_label, "position", start_position + Vector2(1.0, 0), 0.03)
+	tween.tween_property(_time_cost_label, "position", start_position, 0.04)
+	tween.parallel().tween_property(_time_cost_label, "modulate", flash_color, 0.06)
+	tween.tween_property(_time_cost_label, "modulate", base_color, 0.16)
